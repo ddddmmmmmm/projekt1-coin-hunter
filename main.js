85122610ac9adb62011bc5fc25f6d045233f3014
Dana <markova.dana@email.cz>
2022-02-26 19:57:56 +0100

    sipky

--- main.js
+++ main.js
@@ -7,3 +7,35 @@ if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || pan
 
 
 // sem začni psát svůj program
+
+
+
+
+
+let myId = document.querySelector('#panacek');
+
+let novaLeva = null;
+let novaNahoru = null;
+
+document.querySelector("body").addEventListener("keydown", function (event) {
+	if (event.key === "ArrowLeft") {
+		novaLeva = novaLeva - 10
+		myId.style.left = novaLeva + "px"
+
+	} else if (event.key === "ArrowRight") {
+		novaLeva = novaLeva + 10
+		myId.style.left = novaLeva + "px"
+
+	} else if (event.key === "ArrowUp") {
+		novaNahoru = novaNahoru - 10
+		myId.style.top = novaNahoru + "px"
+
+	} else if (event.key === "ArrowDown") {
+		novaNahoru = novaNahoru + 10
+		myId.style.top = novaNahoru + "px"
+	}
+
+})
+
+
+
